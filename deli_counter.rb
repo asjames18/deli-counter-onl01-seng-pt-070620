7a10322568a78aa6ecff0b9932425006cbbc27af
Antonio James <asjames18@gmail.com>
2020-07-13 18:33:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,6 @@ def take_a_number(katz_deli, name)
    katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
-end
 def now_serving(katz_deli)
   katz_deli.size == 0
   puts "There is nobody waiting to be served!"
