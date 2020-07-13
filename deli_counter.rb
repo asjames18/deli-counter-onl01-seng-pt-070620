98cb0581275cae2c8790c28d8b41bea1d20465ee
Antonio James <asjames18@gmail.com>
2020-07-13 17:22:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,12 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
-    sentence = "The line is currently:"
+    sentence1 = "The line is currently:"
     katz_deli.each.with_index(0) do |people, index|
       index += 1
-      sentence << "#{index}, #{people}"
+      sentence << " #{index}, #{people}"
     end
-    puts "#{sentence}"
+    puts "#{sentence1}"
   end
 end
 
