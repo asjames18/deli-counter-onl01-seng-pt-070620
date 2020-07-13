c685d3d0f01d27de39239184f90bfbed0584128e
Antonio James <asjames18@gmail.com>
2020-07-13 18:26:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
     sentence1 = "The line is currently:"
-    katz_deli.each.with_index(0) do |people, index|
+    katz_deli.length(0) do |people, index|
       index += 1
       sentence1 << " #{index}. #{people}"
     end
@@ -13,10 +13,8 @@ end
 
 def take_a_number(katz_deli, name)
    katz_deli.push(name)
-   counter = 1
-  katz_deli.each_with_index do |name|
-    puts "Welcome, #{name}. You are number #{counter} in line."
-    counter += 1
+  katz_deli.each do |name|
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 end
 
