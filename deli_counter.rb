ec6bc38dfaefae48188206618ae68f9f9ce50464
Antonio James <asjames18@gmail.com>
2020-07-13 18:16:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,10 @@ end
 
 def take_a_number(katz_deli, name)
    katz_deli.push(name)
-  #  counter = 1
-  katz_deli.each_with_index  |name|
+   counter = 1
+  katz_deli.each_with_index do |name|
     puts "Welcome, #{name}. You are number #{counter} in line."
-    # counter += 1
+    counter += 1
   end
 end
 
