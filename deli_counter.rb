3139638edac4fa1147205fdace4177660a8b6e69
Antonio James <asjames18@gmail.com>
2020-07-13 18:06:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,16 @@ end
 
 def take_a_number(katz_deli, name)
    katz_deli.push (name)
-   counter = 4
+   counter = 1
   katz_deli.each_with_index do |name|
     puts "Welcome, #{name}. You are number #{counter} in line."
-    counter += 0
+    counter += 1
   end
+
+def now_serving(katz_deli)
+  katz_deli.size == 0
+  puts "there is nobody in line should."
+else
+  puts "Currently serving #{katz_deli[0]}"
+  katz_deli.unshift
 end
