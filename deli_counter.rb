5fd8993575693593237f746abf76a966490a71cb
Antonio James <asjames18@gmail.com>
2020-07-13 17:26:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,14 +5,14 @@ def line(katz_deli)
     sentence1 = "The line is currently:"
     katz_deli.each.with_index(0) do |people, index|
       index += 1
-      sentence << " #{index}, #{people}"
+      sentence1 << " #{index}, #{people}"
     end
     puts "#{sentence1}"
   end
 end
 
 def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index} in line."
+   "Welcome, #{name}. You are number #{katz_deli.index} in line."
   katz_deli.each_with_index do |index|
     index += 1
     puts "Welcome, #{name}. You are number #{index} in line."
